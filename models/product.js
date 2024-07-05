e3a0d360fd6b94abec80a5dc1e2e260b242c7e42
ashiq <ashiqtom6@gmail.com>
2024-07-05 15:15:43 +0530

    Integrating CRUD with MongoDB

--- models/product.js
+++ models/product.js
@@ -1,22 +1,69 @@
+const mongodb = require('mongodb');
 const {getDb}=require('../util/database');
 
 class Product{
-    constructor(title,price,description,imageURL){
+    constructor(title,price,description,imageURL,id){
         this.title=title;
         this.price=price;
         this.description=description;
         this.imageURL=imageURL;
+        this._id= id ? new mongodb.ObjectId(id) : null;
     }
     save(){ 
         const db=getDb();
-        return db.collection('products')
-            .insertOne(this)
+        let dbOp;
+        if(this._id){
+            dbOp=db.collection('products').updateOne({_id:this._id},{$set:this});
+        } else {
+            dbOp=db.collection('products').insertOne(this)
+        }
+        return dbOp
             .then(result=>{
-                console.log(result); 
+                // console.log(result); 
             })
             .catch(err=>{
                 console.log(err)
             }) 
+    }
+
+    static fetchAll(){
+        const db=getDb();
+        return db
+            .collection('products')
+            .find()
+            .toArray()
+            .then(product=>{
+                console.log(product);
+                return product;
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+    }
+
+    static findById(prodId){
+        const db=getDb();
+        return db
+            .collection('products')
+            .find({_id: new mongodb.ObjectId(prodId)})
+            .next()
+            .then(product=>{
+                return product;
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+    }
+    static deleteById(prodId){
+        const db=getDb();
+        return db.collection('products')
+        .deleteOne({_id: new mongodb.ObjectId(prodId)})
+        .then(res=>{
+            console.log('deleted')
+        })
+        .catch(err=>{
+            console.log(err)
+        });
     }
 }
 
